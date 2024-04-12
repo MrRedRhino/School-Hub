5e7e3e7b666784f704f40547a9e2c6e351829ba5
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2024-04-12 19:03:26 +0200

    Fix ms formattingShow actual date of test

--- frontend/src/pages/home/CountdownWidget.vue
+++ frontend/src/pages/home/CountdownWidget.vue
@@ -1,21 +1,20 @@
 <script setup>
-
 import Widget from "@/pages/home/Widget.vue";
 import Countdown from "@/components/Countdown.vue";
 </script>
 
 <template>
   <Widget title="Abi-Countdown">
-    <h1>Französisch: </h1>
+    <h1>Französisch: <a>(22.04.2024)</a></h1>
     <Countdown date="2024-04-22 08:15"></Countdown>
 
-    <h1>Deutsch: </h1>
+    <h1>Deutsch: <a>(25.04.2024)</a></h1>
     <Countdown date="2024-04-25 08:15"></Countdown>
 
-    <h1>3. Fach: </h1>
+    <h1>3. Fach: <a>(03.05.2024)</a></h1>
     <Countdown date="2024-05-03 08:15"></Countdown>
 
-    <h1>Mathe: </h1>
+    <h1>Mathe: <a>(07.05.2024)</a></h1>
     <Countdown date="2024-05-07 08:00"></Countdown>
   </Widget>
 </template>
@@ -24,5 +23,10 @@ import Countdown from "@/components/Countdown.vue";
 h1 {
   font-size: 20px;
   margin-bottom: 0;
+}
+
+a {
+  color: var(--text-mute);
+  font-weight: normal;
 }
 </style>
